9ce22c49cea98e466adb85d90a05a39259a3be08
benja201020102010 <166016868+benja201020102010@users.noreply.github.com>
2024-04-04 08:27:39 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+console.log("la suma es"+ 1+1)
+console.log("la resta es" + 6-2)  
+console.log("ladivision es"+ 4/2)
+console.log("la multiplicacion es" + 4*2)
+//punto 3
+let  numero1= 2
+let numero2=4
+let numero3=2
+let numero4=8
+alert(numero1)
+alert(numero2) 
+alert(numero3) 
+alert(numero4) 
+//punnto 4
+let añoactual=2024
+let añonacimietno=2010
+alert("la edad es"+añoactual-añonacimietno) 
+//punto 7
+ let nombreAlumno = "benjamin"
+ let apellidoAlumno = "armitano"
+ console.log("bienvenido"+nombreAlumno+apellidoAlumno) 
+ alert("bienvenido"+nombreAlumno+apellidoAlumno) 
+ //punto 5
+  añoactual=prompt("ingresa el año actual")
+  añonacimietno=prompt("ingresa tu año de nacimiento") 
+//punto6
+let nombre =prompt("ingresa tu nombre") 
+alert("bienvenido" + nombre) 
+//punto9
+let diez=10
+let tres= 3
+let resultadoMultplicacion=10*3 
+console.log(resultadoMultplicacion) 
+
